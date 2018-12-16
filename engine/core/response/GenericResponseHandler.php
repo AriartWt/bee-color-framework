d791facc85de494130ab54aef813b390f45c5ba7
Jordan Breton <ariartwt@gmail.com>
2018-12-16 13:54:53 +0100

    Added : a GenericResponseHandler to skip simple response handler implementations

---
+++ engine/core/response/GenericResponseHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace wfw\engine\core\response;
+
+/**
+ * CHandler générique qui charge une vue passée en paramètre du constructeur.
+ */
+final class GenericResponseHandler extends DefaultResponseHandler {}
